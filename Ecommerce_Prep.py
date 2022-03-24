cba7459f9465a5527592a44e7ffb40a7c5b680cd
Adam Resnick <a.rezzy@gmail.com>
2022-03-23 21:35:33 -0500

    Fixing orders

--- Ecommerce_Prep.py
+++ Ecommerce_Prep.py
@@ -7,7 +7,7 @@ Created on Mon Feb  7 18:43:07 2022
 
 import pandas as pd
 pd.set_option('display.max_columns', 500)
-orders = pd.read_csv("orders_dataset.csv")
+orders = pd.read_csv("Orders.csv")
 
 orders
 
@@ -25,4 +25,4 @@ orders["estimated_days_to_deliver"] = (orders["order_estimated_delivery_date"]  
 
 orders["delivery_time_category"] = pd.qcut(orders["estimated_days_to_deliver"], 5, labels=["Very Short", "Short", "Normal", "Long","Very Long"])
 
-orders.to_csv("orders_dataset_new.csv")
+orders.to_csv("Orders.csv")
